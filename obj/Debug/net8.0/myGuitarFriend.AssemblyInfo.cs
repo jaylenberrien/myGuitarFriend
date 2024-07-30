530496916cfb38b6561fee9598b9d39e944ff6b8
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-07-30 09:55:52 -0400

    added basic structure for the functionality of the notes pagess

--- obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
+++ obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef7ef3b92f8f1c37a0fad2911e6912e811f21e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c4b57b635851831576c5414f7b285c8bc35a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

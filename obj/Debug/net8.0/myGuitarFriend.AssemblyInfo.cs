ffcf920845d38c1d44318fbf3822e80794da5f7e
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-07-31 09:51:00 -0400

    added more button and made a basic login page

--- obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
+++ obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537639a0ca536807553486181800f384eca8945d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1e1c3d818048c0890b200bc75055a5d54c2071")]
 [assembly: System.Reflection.AssemblyProductAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

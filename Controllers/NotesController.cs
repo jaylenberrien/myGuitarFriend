139bd4c45b177d164f1dbcd16f4eff9c14b88945
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-08-07 09:42:59 -0400

    commented out old mci audio code

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -14,10 +14,6 @@ namespace myGuitarFriend.Controllers;
 public class NotesController: Controller
 { 
 
-  [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
-  private static extern int myfunc(string a, string b, int c, int d);
-
-
   private readonly ILogger<NotesController> _logger;
 
   public NotesController(ILogger<NotesController> logger)
@@ -26,31 +22,46 @@ public class NotesController: Controller
   }
 
 
- private void MciErrorCheck(int result)
-  {
-    if(result != 0)
-    {
-      StringBuilder errorText = new StringBuilder(256);
-      mciGetErrorString(result, errorText, errorText.Capacity);
-      _logger.LogError($"MCI Error: {errorText}");
-      throw new InvalidOperationException(errorText.ToString());
-    }
-  }
-
-  [DllImport("winmm.dll", CharSet = CharSet.Ansi)]
-  private static extern int mciGetErrorString(int errorCode, StringBuilder errorText, int errorTextSize);
-
   public IActionResult startRec()
   {
-    int result = myfunc("open new Type waveaudio Alias recsound","",0,0);
-    MciErrorCheck(result);
-    result = myfunc("record recsound","",0,0);
-    MciErrorCheck(result);
-    return Content("recording started");
+
   }
 
   public IActionResult endRec()
   {
+
+  }
+ 
+}
+
+
+
+//OLD MCI INPUT CODE
+
+  // [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+  // private static extern int myfunc(string a, string b, int c, int d);
+
+
+
+//  private void MciErrorCheck(int result)
+//   {
+//     if(result != 0)
+//     {
+//       StringBuilder errorText = new StringBuilder(256);
+//       mciGetErrorString(result, errorText, errorText.Capacity);
+//       _logger.LogError($"MCI Error: {errorText}");
+//       throw new InvalidOperationException(errorText.ToString());
+//     }
+//   }
+
+  // [DllImport("winmm.dll", CharSet = CharSet.Ansi)]
+  // private static extern int mciGetErrorString(int errorCode, StringBuilder errorText, int errorTextSize);
+
+      // int result = myfunc("open new Type waveaudio Alias recsound","",0,0);
+    // MciErrorCheck(result);
+    // result = myfunc("record recsound","",0,0);
+    // MciErrorCheck(result);
+    // return Content("recording started");
 
     // laptop version
 
@@ -61,12 +72,8 @@ public class NotesController: Controller
     // Pc version
 
     //the line below is why there is an issue with it saving and ending (i have to clikc it more than once. even then it doesnt save)
-    int result = myfunc(@"save recsound C:\Users\Jaylen Berrien\Desktop\test.wav","",0,0);
-    MciErrorCheck(result);
-    result = myfunc("close recsound","",0,0);
-    MciErrorCheck(result);
-    return Content("recording ended");
-  }
-
- 
-}
+    // int result = myfunc(@"save recsound C:\Users\Jaylen Berrien\Desktop\test.wav","",0,0);
+    // MciErrorCheck(result);
+    // result = myfunc("close recsound","",0,0);
+    // MciErrorCheck(result);
+    // return Content("recording ended");

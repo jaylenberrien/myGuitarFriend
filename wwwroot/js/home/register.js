42191ee218a2065d8a6cfb12bbd0e241959fdd93
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-09-21 16:34:07 -0400

    saved new changes

--- wwwroot/js/home/register.js
+++ wwwroot/js/home/register.js
@@ -1,19 +1,4 @@
 function registerUser(event){
-<<<<<<< HEAD
-fetch('/Account/Register')
-     .then(response => response.json())
-     .then(newUser =>{
-          event.preventDefault();
-          const username = document.getElementById("username-inp");
-          const usernameValue = username.value;
-          console.log(usernameValue);
-          const password = document.getElementById("password-inp");
-          const passwordValue = password.value;
-          console.log(passwordValue);
-          console.log(newUser);
-     })
-     .catch(error => console.error('Error: ', error));  
-=======
     event.preventDefault();
     // fetch('/Account/Register')
     // .then(response => response.json())
@@ -29,7 +14,6 @@ fetch('/Account/Register')
 
     // console.log(newUser);
     console.log("the js page is connected and working");     
->>>>>>> b21cfafe86d46bcc29537b128e7f764e564e36a4
 }
 
   

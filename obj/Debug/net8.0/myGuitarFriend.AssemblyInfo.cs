e6b12465da5dead2cc7ff30449829d1804a740db
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-09-20 20:03:11 -0400

    merge attempt after cleaning

--- obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
+++ obj/Debug/net8.0/myGuitarFriend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5cc1cbda90d7fc66b820eee13f147424186a66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f1253f74bebb968f382e310a28fca4eee36fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myGuitarFriend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/home/register.js
+++ wwwroot/js/home/register.js
@@ -1,29 +1,16 @@
 function registerUser(event){
-    // fetch('/Account/Register')
-	//  .then(response => response.json())
-       // .then(newUser =>{
-            // event.preventDefault();
-       //     const username = document.getElementById("username-inp");
-       //     const usernameValue = username.value;
-       //     console.log(usernameValue);
-       //     const password = document.getElementById("password-inp");
-       //     const passwordValue = password.value;
-       //     console.log(passwordValue);
-       //     console.log(newUser);
-       // })
-       // .catch(error => console.error('Error: ', error));
-
-            const username = document.getElementById("username-inp");
-            const usernameValue = username.value;
-            console.log(usernameValue);
-            const password = document.getElementById("password-inp");
-            const passwordValue = password.value;
-            console.log(passwordValue);
-            console.log(newUser);
-
-
-
-
-  
+fetch('/Account/Register')
+     .then(response => response.json())
+     .then(newUser =>{
+          event.preventDefault();
+          const username = document.getElementById("username-inp");
+          const usernameValue = username.value;
+          console.log(usernameValue);
+          const password = document.getElementById("password-inp");
+          const passwordValue = password.value;
+          console.log(passwordValue);
+          console.log(newUser);
+     })
+     .catch(error => console.error('Error: ', error));  
 }
 
